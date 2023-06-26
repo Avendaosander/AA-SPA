776b19eb2afd5b6568c258c8329de745900f6aad
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-06-26 16:29:35 -0400

    Contexto actualizado

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -4,11 +4,19 @@ export const UserContext = createContext()
 
 export function UserProvider ({ children }) {
    const [user, setUser] = useState(null)
+   const [adminMode, setAdminMode] = useState(false)
+   
+   const toggleAdminMode = () => {
+      setAdminMode(!adminMode)
+   }
 
    return (
       <UserContext.Provider value={{
          user,
-         setUser
+         setUser,
+         adminMode,
+         setAdminMode,
+         toggleAdminMode
       }}>
          {children}
       </UserContext.Provider>
