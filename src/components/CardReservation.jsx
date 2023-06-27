506e1cc7a8a081dbfbf7256c2654fb66a7bf353c
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-06-26 22:59:40 -0400

    Card Modificada

--- src/components/CardReservation.jsx
+++ src/components/CardReservation.jsx
@@ -4,12 +4,20 @@ function CardReservation({ data, handleModalEdit, handleModalDelete }) {
       <article className='flex flex-col gap-3 p-5 bg-emerald-300 ring-2 ring-emerald-600 rounded-xl max-w-[300px] w-full mx-auto text-lg'>
          <h3 className='font-bold text-center text-xl'>{data.service.titulo}</h3>
          <div className="flex items-center gap-2 font-medium">
+            <p>Responsable:</p>
+            <p className='text-sm'>{data.nombre}</p>
+         </div>
+         <div className="flex items-center gap-2 font-medium">
             <p>Fecha:</p>
             <p className='text-sm'>{data.fecha}</p>
          </div>
          <div className="flex items-center gap-2 font-medium">
             <p>Hora:</p>
-            <p className='text-sm'>{data.hora} pm</p>
+            <p className='text-sm'>{data.hora}</p>
+         </div>
+         <div className="flex items-center gap-2 font-medium">
+            <p>Duración:</p>
+            <p className='text-sm'>{data.duracion} min</p>
          </div>
          <div className="flex items-center gap-2 font-medium">
             <p>Personas:</p>
