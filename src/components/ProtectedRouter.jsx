f42c1074788146d188524758cbdaff02fe5355b0
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-06-27 22:12:59 -0400

    Correcion

--- src/components/ProtectedRouter.jsx
+++ src/components/ProtectedRouter.jsx
@@ -1,13 +1,11 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { SpaProvider } from '../context/spaContext'
-import ErrorModal from './ErrorModal'
 
 function ProtectedRouter({ isAllowed, children, redirectTo = '/login'}) {
    if (!isAllowed) return <Navigate to={redirectTo}/> 
    return (
       <SpaProvider>
          {children ? children : <Outlet/>}
-         <ErrorModal/>
       </SpaProvider>
    )
 }
